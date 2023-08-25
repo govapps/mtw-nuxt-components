fbcbaca842964dbc94f458312e07b853354c9347
yasmmin <ycfrenchgirl2@gmail.com>
2023-08-24 22:11:28 -0300

    adição de component para select

--- components/Input.vue
+++ components/Input.vue
@@ -1,11 +1,16 @@
 <template>
   <label class="text-left block text-sm font-medium leading-6 text-gray-900">{{ label }}</label>
   <div class="mt-2">
+    <ClientOnly>
     <component
+      :is="variant ?? 'input'"
       :id="id"
+      :ref="(el: any) => {
+        if(el && blockPaste) el.onpaste = (e: Event) => e.preventDefault();
+        if(el && blockCopy) el.oncopy = (e: Event) => e.preventDefault();
+      }"
       :name="name"
-      :is="variant"
-      :type="type"
+      :type="type ?? 'text'"
       :placeholder="placeholder"
       :class="`
         block
@@ -29,119 +34,68 @@
         ${disabled ? 'disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200' : ''}
       `"
       :value="value"
-      @input="_onChange($event)"
-      @blur="_onBlur($event)"
-
       :disabled="disabled"
 
-      :ref="(el: any) => {
-        if(el && blockPaste) el.onpaste = (e: Event) => e.preventDefault();
-        if(el && blockCopy) el.oncopy = (e: Event) => e.preventDefault();
-      }"
-    />
+      @input="_onChange($event)"
 
-    <p v-if="showHelperText || _showHelperText" class="text-sm mt-1" :class="{'text-red-500': isError || _isError}">{{ helperText }}</p>
+      @blur="_onBlur($event)"
+    />
+    </ClientOnly>
+
+    <p
+      v-if="showHelperText || _showHelperText"
+      class="text-sm mt-1"
+      :class="{'text-red-500': isError || _isError}"
+    >
+      {{ helperText }}
+    </p>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useValidationsBR, } from "validations-br";
+import { ref, onMounted, onUpdated, } from "vue";
 
 const _value = ref("");
 const _isError = ref(false);
 const _showHelperText = ref(false);
 
-const props = defineProps({
-  variant: {
-    type: String,
-    default: "input"
-  },
-  id: { type: String },
-  name: { type: String },
-  value: {
-    type: String,
-    default: ""
-  },
-  label: {
-    type: String,
-    default: "",
-  },
-  type: {
-    type: String,
-    default: "text"
-  },
-  placeholder: {
-    type: String,
-    default: ""
-  },
-  isError: {
-    type: Boolean,
-    default: false
-  },
-  disabled: {
-    type: Boolean,
-    default: false
-  },
-  showHelperText: {
-    type: Boolean,
-    default: false
-  },
-  helperText: {
-    type: String,
-    default: ""
-  },
-  onChange:{
-    type: Function,
-    default: (event: any) => {}
-  },
-  onBlur:{
-    type: Function,
-    default: (event: any) => {}
-  },
-  validateWithOnBlur: {
-    type: Boolean,
-    default: false
-  },
-  validateWhen:{
-    type: Function,
-    default: undefined
-  },
-  validate:{
-    type: [Function, String],
-    default: undefined
-  },
-  onValidate:{
-    type: Function,
-    default: undefined
-  },
-  validateOnUpdate: {
-    type: Boolean,
-    default: false
-  },
-  blockPaste:{
-    type: Boolean,
-    default: false
-  },
-  blockCopy:{
-    type: Boolean,
-    default: false
-  },
-});
+const props = defineProps<{
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onChange?:(value: string, event: any) => void;
+  variant?: string;
+  id?: string;
+  name?: string;
+  value?: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  isError?: boolean;
+  disabled?: boolean;
+  showHelperText?: boolean;
+  helperText?: string;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onBlur?: (value: string, event: any) => void;
+  validateWithOnBlur?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  validateWhen?: (value: string) => boolean;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  validate?: (value: string) => boolean;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onValidate?: (isError: boolean) => void;
+  validateOnUpdate?: boolean;
+  blockPaste?: boolean;
+  blockCopy?: boolean;
+}>();
 
 onMounted(() => {
-  _value.value = props.value;
+  _value.value = props?.value ?? "";
 });
 
-function _onValidate(){
-  if(props.onValidate && props.validate){
+function _onValidate () {
+  if (props.onValidate && props.validate) {
     let error = true;
 
-    if(typeof props.validate === "string"){
-      if(useValidationsBR(props.validate, _value.value)){
-        error = false;
-      }
-    }
-    else if(props.validate(_value.value)){
+    if (props.validate(_value.value)) {
       error = false;
     }
 
@@ -152,32 +106,32 @@ function _onValidate(){
   }
 }
 
-function _onChange(event: any){
+function _onChange (event: any) {
   _value.value = event.target.value;
 
-  if(props?.onChange){
+  if (props?.onChange) {
     props?.onChange(event.target.value, event);
 
-    if(props.onValidate){
-      if(props.validateWhen && props.validateWhen(_value.value)){
+    if (props.onValidate) {
+      if (props.validateWhen && props.validateWhen(_value.value)) {
         _onValidate();
       }
     }
   }
 }
 
-function _onBlur(event: any){
-  if(props.onBlur){
+function _onBlur (event: any) {
+  if (props.onBlur) {
     props?.onBlur(event.target.value, event);
 
-    if(props.validateWithOnBlur){
+    if (props.validateWithOnBlur) {
       _onValidate();
     }
   }
 }
 
 onUpdated(() => {
-  if(props.validateOnUpdate){
+  if (props.validateOnUpdate) {
     _onValidate();
   }
 });
