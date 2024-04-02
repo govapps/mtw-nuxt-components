c45818610767b9ae273db1e1b519049c7cbafdbe
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2024-04-02 13:59:02 -0300

    refactor: refatorado layout do button

--- components/Button.vue
+++ components/Button.vue
@@ -8,19 +8,20 @@
   <button
     v-else
     :class="`
-      m-1
-      px-3
-      py-2
-      text-sm
-      shadow-sm
+      inline-flex
+      items-center
+      justify-center
+      gap-2.5
+      py-4
+      px-10
+      text-center
+      font-medium
+      lg:px-8
+      xl:px-10
+      bg-primary
+      text-white
       rounded-md
-      font-semibold
-      hover:bg-primary-500
-      focus-visible:outline
-      focus-visible:outline-2
-      focus-visible:outline-offset-2
-      items-center gap-x-2
-      focus-visible:outline-primary-600
+      ${!disabled ? 'hover:bg-opacity-90': '!bg-[#EFF4FB] !text-[#64748B]'}
       ${hasIcon ? 'inline-flex' : ''}
       ${isWidthFull ? 'w-full' : ''}
       ${!secondary ? 'bg-primary-600 text-white' : ''}
