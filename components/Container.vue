1c76f1458468518df2a557347d744086f1f12c49
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2024-04-02 17:27:50 -0300

    feat: adicionado prop title para colocar um titulo no container

--- components/Container.vue
+++ components/Container.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+    <div v-if="props.title" class="border-b border-stroke px-4 py-4 dark:border-strokedark sm:px-6 xl:px-7.5">
+      <h3 class="font-medium text-black dark:text-white">
+        {{ props.title }}
+      </h3>
+    </div>
     <slot />
   </div>
 </template>
+
+<script lang="ts" setup>
+
+const props = defineProps<{
+  title?: string;
+}>();
+
+</script>
