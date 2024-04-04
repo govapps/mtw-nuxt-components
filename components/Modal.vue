409c25ab6d4b9cb4ce2364d36368c107f8c90328
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2024-04-04 15:17:36 -0300

    refactor: refatorado layout modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -5,7 +5,7 @@
   >
     <Dialog
       as="div"
-      class="relative z-10"
+      class="fixed top-0 left-0 z-999999 flex h-full min-h-screen w-full items-center justify-center bg-black/90 px-4 py-5"
       @close="onClose()"
     >
       <TransitionChild
@@ -31,34 +31,21 @@
             leave-from="opacity-100 translate-y-0 sm:scale-100"
             leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-              <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
-                <button
-                  type="button"
-                  class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
-                  @click="onClose()"
-                >
-                  <span class="sr-only">Close</span>
-                  <XMarkIcon
-                    class="h-6 w-6"
-                    aria-hidden="true"
-                  />
-                </button>
+            <DialogPanel class="w-full max-w-142.5 rounded-lg bg-white py-12 px-8 text-center dark:bg-boxdark md:py-15 md:px-17.5">
+              <span v-if="hasIcon" class="mx-auto inline-block">
+                <slot name="icon" />
+              </span>
+              <DialogTitle
+                as="h3"
+                class="mt-5.5 pb-2 text-xl font-bold text-black dark:text-white sm:text-2xl"
+              >
+                <slot name="title" />
+              </DialogTitle>
+              <div class="mb-10 font-medium">
+                <slot name="body" />
               </div>
 
-              <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                <DialogTitle
-                  as="h3"
-                  class="text-base font-semibold leading-6 text-gray-900"
-                >
-                  <slot name="title" />
-                </DialogTitle>
-                <div class="mt-2">
-                  <slot name="body" />
-                </div>
-              </div>
-
-              <div class="mt-5 sm:mt-4">
+              <div class="-mx-3 flex flex-wrap gap-y-4 ">
                 <slot name="footer" />
               </div>
             </DialogPanel>
@@ -70,12 +57,12 @@
 </template>
 
 <script lang="ts" setup>
-import { XMarkIcon, } from "@heroicons/vue/24/outline";
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot, } from "@headlessui/vue";
 
 defineProps<{
   onClose:() => void;
   isOpen: boolean;
+  hasIcon?: boolean;
 }>();
 
 </script>
